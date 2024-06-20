099bf07d7b91db16fa4cb25fbc26ea152013326f
oseiasdfarias <oseias.dfarias@gmail.com>
2024-06-20 13:57:34 -0300

    feat: add aula 2, criado mais um endpoit olamundo que retorna um html.

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,4 +11,22 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get("/")  # Act (Ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
-    assert response.json() == {"message": "Olá Mundo!"}   # Assert
+    assert response.json() == {"message": "Olá Mundo!"}  # Assert
+
+
+def test_ola_mundo_deve_retornar_ok_e_html():
+    client = TestClient(app)  # Arrange (Organização do teste)
+
+    response = client.get("/olamundo")  # Act (Ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
+    print(response.text)
+    assert response.text == """
+        <html>
+            <head>
+                <title> FastAPI </title>
+            </head>
+            <body>
+                <h1> Olá, Mundo! <h1>
+            </body>
+        </html> """   # Assert

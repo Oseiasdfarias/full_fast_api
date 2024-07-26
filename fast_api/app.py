36a1d1a5c456c2b2dc4c1b473898e6c44816dd03
oseiasdfarias <oseias.dfarias@gmail.com>
2024-07-25 21:08:46 -0300

    feat: primeira migração com Alembic. criada tabela de usuários

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,7 +1,6 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI, HTTPException
-
 from fast_api.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
@@ -28,7 +27,6 @@ def read_users():
 
 @app.put("/users/{user_id}", response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
-
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="User not found."
@@ -43,7 +41,6 @@ def update_user(user_id: int, user: UserSchema):
 
 @app.delete("/users/{user_id}", response_model=Message)
 def delete_user(user_id: int):
-
     if user_id < 1 or user_id > len(database):
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail="User not found."

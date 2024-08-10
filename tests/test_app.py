c34f6d262e0d887f333f8c965b31a6bc6dc44caa
oseiasdfarias <oseias.dfarias@gmail.com>
2024-08-10 19:21:44 -0300

    feat: atualizando arquivos .ymal

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,4 +5,4 @@ def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get("/")  # Act (Ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
-    assert response.json() == {"message": "Olá Mundo!"}  # Assert
+    assert response.json() == {"message": "Hello World!"}  # Assert
